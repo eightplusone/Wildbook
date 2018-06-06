717247db31a43c3388552247fb65121ed97cba58
Hai Tran <hai.t@outlook.com>
2018-06-06 11:52:48 -0500

    Able to get current location using Geolocation API
    Still unsure if we need to eject to files at the moment to get into .gradle files.

--- App.js
+++ App.js
@@ -4,24 +4,47 @@ import {StyleSheet, Button, Text, View} from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+      }
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
+          },
+          error: null,
+        });
+      },
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <MapView style={styles.map}
-          region={{
-            latitude: 59.32932349999999,
-            longitude: 18.06858000000063,
-            latitudeDelta: 0.1,
-            longitudeDelta: 0.1
-          }}
+          region={this.state.region}
         >
 
           <MapView.Marker
             coordinate={{
-              latitude: 59.32932349999999,
-              longitude: 18.06858000000063
+              latitude: this.state.region.latitude,
+              longitude: this.state.region.longitude,
             }}
-            title={'My marker title'}
+            title={this.state.region.latitude.toString() + ", " + this.state.region.longitude.toString()}
             description={'My marker description'}
             onPress={() => {
               /* 1. Navigate to the Details route with params */
@@ -39,6 +62,30 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
   render() {
     /* 2. Get the param, provide a fallback value if not available */
     const { navigation } = this.props;
@@ -50,6 +97,9 @@ class DetailsScreen extends React.Component {
         <Text>Details Screen</Text>
         <Text>itemId: {JSON.stringify(itemId)}</Text>
         <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+        <Text>Latitude: {this.state.latitude}</Text>
+        <Text>Longitude: {this.state.longitude}</Text>
+        {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
         <Button
           title="Go to Details... again"
           onPress={() =>
